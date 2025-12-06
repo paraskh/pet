0a21b600b0d1a39943088731485e585bf272f3ce
pk <hackwise@hackwise.io>
2025-12-07 00:28:34 +0800

    Build setup

--- build/apps/app1/CMakeFiles/app1.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-apps/app1/CMakeFiles/app1.dir/src/main.cpp.o: \
-  /workspace/apps/app1/src/main.cpp \
-  /workspace/libs/core/include/core/core.hpp \
-  /workspace/libs/utils/include/utils/utils.hpp
--- build/apps/app2/CMakeFiles/app2.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-apps/app2/CMakeFiles/app2.dir/src/main.cpp.o: \
-  /workspace/apps/app2/src/main.cpp \
-  /workspace/libs/core/include/core/core.hpp
